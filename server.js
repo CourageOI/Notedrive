5fcdd019dca4c844bcae9384795cf5d144a08094
CourageOI <osascourage27@gmail.com>
2022-06-21 18:12:29 +0100

    ready to deploy

--- server.js
+++ server.js
@@ -18,9 +18,9 @@ app.use(errorHandler);
 const path = require("path");
 
 if (process.env.NODE_ENV === "production") {
-  app.use("/", express.static(path.resolve(__dirname, "../client/build")));
+  app.use("/", express.static("client/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client/build/index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
