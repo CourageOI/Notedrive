a80765c12e0070d86921954667054aabe50be57f
Osakpolor Courage Ihensekhien <61490258+CourageOI@users.noreply.github.com>
2022-06-21 14:19:43 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -16,5 +16,13 @@ app.use("/api/notes", noteRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
+if(process.env.NODE_ENV==='production'){
+    app.use('/', express.static('client/build'))
+    app.get('*', (req, res)=>{
+        res.sendFile(path.resolve(__dirname, 'client/build/index.html'))
+    })
+}
+
+
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, console.log(`Server running on Port ${PORT}`));
